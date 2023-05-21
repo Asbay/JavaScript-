58a5f4ec39039dd3795fc6d3b9ab954a1d1ddc99
AsBay <asenabayhan@outlook.de>
2023-05-21 22:08:19 +0200

    21.05

--- day15/js/script-82.js
+++ day15/js/script-82.js
@@ -1,16 +1,18 @@
 const frmLogin = document.getElementById("frmLogin");
 
 frmLogin.addEventListener("submit", (e) => {
-    e.preventDefault(); // formun submit davranışını iptal eder.
-
+    e.preventDefault(); // formun submit davranışını iptal eder.yazilan ne ise butona basinca hemen gider
+    //onu engellemek icin preventDefault() u kullandik
+//kullanicidan bilgileri aliyoruz:
     const txtEmail = document.getElementById("txtEmail");
     const txtPassword = document.getElementById("txtPassword");
 
     try {
-        const email = txtEmail.value;
+         const email = txtEmail.value;
         const password = txtPassword.value;
-
-        if(!email || !isEmail(email)) throw new Error("Please enter a valid email"); 
+//eger email degilse yani bossa undefined saya da null sa  yada email formatinda degilse(@isareti var mi noktasi varmi)
+//regex rgular expression bi kalip hazirlayip ona göre kontrol edevbiliriz
+        if(!email || !isEmail(email)) throw new Error("Please enter a valid email"); //throw hata firlatmak
         if(!password) throw new Error("Please enter your password"); 
 
         e.target.submit();
@@ -26,6 +28,8 @@ frmLogin.addEventListener("submit", (e) => {
 
 
 const isEmail = (email) => {
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return emailRegex.test(email);
+    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;//regexr.comdan aldik
+    //regex iki / arasina yazilir kural bu
+    return emailRegex.test(email);//test methodu regex isleminin bir methodu burda kendisi kontrol etmis oluyor##
+    //verilen kurala uygun mu degil mi diye
 }
